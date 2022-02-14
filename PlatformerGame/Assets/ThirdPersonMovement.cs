30108f916f747e334689dc4e64f3cafb0ab1b5d7
Trevor Jedziniak <trevjed@gmail.com>
2022-02-13 20:26:18 -0700

    gravity a
    added player gravity, commented out attempt at backwards movement

--- PlatformerGame/Assets/ThirdPersonMovement.cs
+++ PlatformerGame/Assets/ThirdPersonMovement.cs
@@ -9,11 +9,27 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform cam;
 
     public float speed = 6f;
+    public float gravity = -9.81f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -26,7 +42,23 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            
         }
+        /* else if (direction.magnitude <= 0.1f)
+        {
+         
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f ,angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            
         
+        } */
+        velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
     }
 }
